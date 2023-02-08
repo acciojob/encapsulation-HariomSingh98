15b1d9eca4ce837fd94d72658e46a15b8ca173ec
HariomSingh98 <70706137+HariomSingh98@users.noreply.github.com>
2023-02-08 22:17:50 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,10 @@
 package com.driver;
 
 public class Main {
+    Scanner sc = new Scanner(System.in);
+      RWOnly rw = new RWOnly();
+      // name has private access in com.driver.RWOnly
+      rw.setName(sc.nextLine());
+      rw.getName();
   
 }
